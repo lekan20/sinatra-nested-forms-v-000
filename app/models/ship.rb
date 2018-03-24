f208ed25274dbea93b13b7f68463b2a7b934624e
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-24 18:15:20 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,9 +1,15 @@
 class Ship
   attr_reader :name, :type, :booty
 
+  SHIPS = []
+
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
+  end
+
+  def self.all
+    SHIPS
   end
 end
