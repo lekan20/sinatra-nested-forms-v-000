94b45764466b9a9dbb29a908d2f0fab53e9c9f8e
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-24 20:06:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ module FormsLab
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
-      # binding.pry
+      binding.pry
       @ships = Ship.all
 
       erb :'pirates/show'
